9be351c44bceeab0ccae2dd32641f1382c7a08a2
akandhare <akandhare@binghamton.edu>
2023-11-21 00:15:49 -0500

    Added logic for n/3

--- large_integer_multiplication.cpp
+++ large_integer_multiplication.cpp
@@ -193,6 +193,94 @@ string divide_and_conquer_multiplication_for_two_small_integers(const string& A,
     return remove_leading_zeros(result);
 }
 
+
+/**
+ * Divides the input strings into three parts and performs multiplication using divide and conquer algorithm.
+ *
+ * @param A the first input string
+ * @param B the second input string
+ *
+ * @return the result of multiplication as a string
+ *
+ * @throws None
+ */
+string divide_and_conquer_multiplication_for_three_small_integers(const string& A, const string& B) {
+
+    int n = max(A.size(), B.size());
+    int mid = n / 3;
+
+    // Base case: if the input strings are small, perform regular multiplication
+    if (n <= 3) {
+        return to_string(stoll(A) * stoll(B));
+    }
+
+    // Divide the input strings into three parts
+    string x = A.substr(0, mid);
+    string y = A.substr(mid, mid);
+    string z = A.substr(2 * mid);
+
+    string a = B.substr(0, mid);
+    string b = B.substr(mid, mid);
+    string c = B.substr(2 * mid);
+
+
+    // Recursive calls for subproblems
+
+    string p1 = divide_and_conquer_multiplication_for_three_small_integers(x, a);
+    string p11 = pad_zeros(p1, 4 * mid);
+
+    string p2_mid = divide_and_conquer_multiplication_for_three_small_integers(y, a);
+    string p2_min = divide_and_conquer_multiplication_for_three_small_integers(x, b);
+    string p2 = string_addition(p2_mid, p2_min);
+
+    //padding zeros for p2
+    string p22 = pad_zeros(p2, 3 * mid);
+
+    string p6 = divide_and_conquer_multiplication_for_three_small_integers(x, c);
+    string p7 = divide_and_conquer_multiplication_for_three_small_integers(y, b);
+    string p8 = divide_and_conquer_multiplication_for_three_small_integers(z,a);
+    
+    //padding zeros for p66 , p77 and p88
+    string p66 = pad_zeros(p6, 2 * mid);
+    string p77 = pad_zeros(p7, 2 * mid);
+    string p88 = pad_zeros(p8, 2 * mid);
+
+    string p3_mid = divide_and_conquer_multiplication_for_three_small_integers(y, c);
+    string p3_min = divide_and_conquer_multiplication_for_three_small_integers(z, b);
+
+    //padding zeros for p33_mid and p33_min
+    string p33_mid = pad_zeros(p3_mid, 1 * mid);
+    string p33_min = pad_zeros(p3_min, 1 * mid);
+
+
+    string p44 = divide_and_conquer_multiplication_for_three_small_integers(z, c);
+
+    std::string result = string_addition(string_addition(string_addition(string_addition(p11, p22), p33_mid), p44), string_addition(p33_min, string_addition(p66, string_addition(p77, p88))));
+
+    return remove_leading_zeros(result);
+
+}
+
+
+/**
+ * Checks the similarity between three strings.
+ *
+ * @param resultOriginal the original string to compare against
+ * @param resultModified the first modified string to compare
+ * @param resultModifiedtwo the second modified string to compare
+ *
+ * @return void
+ *
+ * @throws None
+ */
+void check_similarity( const string& resultModified, const string& resultModifiedtwo) {
+    if (resultModified == resultModifiedtwo) {
+        cout << "They are the same!" << endl;
+    } else {
+        cout << "They are different!" << endl;
+    }
+}
+
 /**
  * Main function that takes command line arguments and performs large integer multiplication using different algorithms.
  *
@@ -229,11 +317,14 @@ int main(int argc, char *argv[]) {
     cout << "A = " << A << endl;
     cout << "B = " << B << endl;
 
-        // Compute A * B using modified divide and conquer algorithm
+    // Compute A * B using modified divide and conquer algorithm
     string resultModified = divide_and_conquer_multiplication_for_two_small_integers(A, B);
     cout << "A*B (The large integer multiplication from the division of two smaller integers is) = " << resultModified << endl;
     A = add_zeros_before_string(A);
     B = add_zeros_before_string(B);
+    string resultModifiedtwo = divide_and_conquer_multiplication_for_three_small_integers(A, B);
+    cout << "A*B (The large integer multiplication from the division of three smaller integers is) = " << resultModifiedtwo << endl;
+    check_similarity( resultModified, resultModifiedtwo);
 
     return 0;
 }
